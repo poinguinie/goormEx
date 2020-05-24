f53d52d004d33ab6c47d37544ca68aebe4adba3d
이지호 <wlgh0206@naver.com>
2020-05-24 10:01:57 +0000

    ver.0.1.5

--- matrix_multi/matrix_multi_code.kt
+++ matrix_multi/matrix_multi_code.kt
@@ -11,21 +11,23 @@ fun main(args: Array<String>) = with(Scanner(System.`in`)) {
 	//You HAVE TO INPUT like this Ex) 3 5 (row : 3, column : 5)
 	//println("row : $rowNumber column : $columnNumber")
 	var rowArray = Array(rowNumber, { 0 })
-	var columnArray = Array(columnNumber, { 0 })
 	for(i in 0..(rowNumber-1)) {
+		var columnArray = Array(columnNumber, { 0 })
 		print("Enter the just ONE row : ")
 		for (j in 0..(columnNumber-1)) {
-			row = row.plus(nextInt())
+			columnArray[j] = nextInt()
 		}
+		rowArray = rowArray.plus(columnArray)
 		//for (j in 0..(columnNumber-1)) {
 		//	row = row.plus(val tmp = nextInt())
 		//}
 		//rowArray = rowArray.plus(row)
 	}
-	for (i in row) {
-		for (j in i) {
+	for (i in rowArray) {
+		println("$i ")
+		/*for (j in i) {
 			print("$j ")
 		}
-		println()
+		println()*/
 	}
 }
